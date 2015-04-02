590fbbe017c4c591a3626f6cefc48c267c4fb198
Fabio Fabrizio <f.fabrizio@syzygy.co.uk>
2015-04-02 14:40:25 +0100

    Angular intellisense as a dev dependency Components style as stylus

--- routes/home.js
+++ routes/home.js
@@ -5,7 +5,6 @@ var Page        = require('../models/page');
 router.route('/')
     .get(function(req, res, next) {
         var pageUrl = "home";
-        console.log(pageUrl);
         Page.findOne({ url: pageUrl }, function(err, page) {
             if (err)
                 res.send(err);
@@ -14,7 +13,7 @@ router.route('/')
                 console.log("Loaded page:", page.id);
                 res.render("index", page);
             } else {
-                var err = new Error('Not Found');
+                err = new Error('Not Found');
                 err.status = 404;
                 next(err);
             }
@@ -23,17 +22,16 @@ router.route('/')
 
 router.route(/^(.*)$/)
     .get(function(req, res, next) {
-        var pageUrl = req.params[0] || "/home";
-        console.log(pageUrl);
-        Page.findOne({ url: pageUrl.substr(1) }, function(err, page) {
+        var pageUrl = (req.params[0] || "/home").substr(1);
+        Page.findOne({ url: pageUrl }, function(err, page) {
             if (err)
                 res.send(err);
 
             if (page !== null) {
-                console.log("Loaded page:", page.id);
+                console.log("Loaded page:", page.url);
                 res.render("index", page);
             } else {
-                var err = new Error('Not Found');
+                err = new Error('Not Found');
                 err.status = 404;
                 next(err);
             }
